58bd39252dbf9d42b123e7e00e248114a208d56c
Lia Va <mistickusya.2012@gmail.com>
2023-10-22 22:51:08 +0200

    move the sdk to a separate repo

--- keys_storage/types.go
+++
@@ -1,9 +0,0 @@
-package keysstorage
-
-import "errors"
-
-var ErrOutOfBound = errors.New("requested key slice is out of the key's bounds")
-
-type Closable interface {
-	Close()
-}
--- keys_storage/utils.go
+++
@@ -1,9 +0,0 @@
-package keysstorage
-
-const DefaultPermMode = 0o600
-
-func SafeClose(obj Closable) {
-	if obj != nil {
-		obj.Close()
-	}
-}
